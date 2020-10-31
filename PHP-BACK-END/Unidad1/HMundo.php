511590d889ebbb1010c68c5a84a0d09ea99ec951
christian <quirogachristian@gmail.com>
2020-10-31 19:13:52 -0300

    subiendo ej oblig PHP BACK END unidad 1

---
+++ PHP-BACK-END/Unidad1/HMundo.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<html lang="es">
+
+<head>
+
+	<meta charset="utf-8">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	  
+	<title>Primera página PHP</title>
+
+</head>
+
+<body>
+
+	<?php 
+		echo '<div style="text-align: center;"><span
+				style="text-decoration: underline;"><h3>Hola Mundo!!</h3></span><br>
+			  </div>';
+	?>
+	
+</body>
+
+</html>
